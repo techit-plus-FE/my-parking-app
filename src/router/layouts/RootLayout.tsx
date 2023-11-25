7abd3cd1e8eeae95ef282d8de465f347e264b1a3
Suam Kang <ksa199653@gmail.com>
2023-11-25 18:05:30 +0900

    docs: 주석 처리

--- src/router/layouts/RootLayout.tsx
+++ src/router/layouts/RootLayout.tsx
@@ -1,12 +1,13 @@
+// import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 // 이 레이아웃은 헤더와 푸터가 적용되지 않은 레이아웃입니다.
 // 모든 페이지에서 적용할 상태나 테마를 여기서 꾸려주어야 합니다. -> 전체 색상테마(다크모드상태), 토큰상태)
 const RootLayout = () => {
-  const [hasFooter, setHasFooter] = useState(false);
-  const [hasHeader, setHasHeader] = useState(false);
-  const [hasFooter, setHasFooter] = useState(false);
-  const [hasFooter, setHasFooter] = useState();
+  // const [hasFooter, setHasFooter] = useState(false);
+  // const [hasHeader, setHasHeader] = useState(false);
+  // const [hasHeaderAndFooter, setHasHeaderAndFooter] = useState(false);
+  // const [hasSearchHeader, setHasSearchHeader] = useState(false);
 
   return (
     <>
