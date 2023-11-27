1c6c957818c875fbe614b66a3e2257510919f34d
suconpa <thkWKddl100@gmail.com>
2023-11-27 14:09:01 +0900

    feat: orderPage 마크업

--- src/components/domain/order-history/detail/OrderHistoryDetailItem.tsx
+++ src/components/domain/order-history/detail/OrderHistoryDetailItem.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const OrderHistoryDetailItem = () => {
-  return <div></div>;
+  return (
+    <div className="order-history-detail-item-container">
+      <div className="order-history-list-wrapper">
+        <div>
+          <img src="https://picsum.photos/200/300" alt="" />
+        </div>
+        <div>
+          <p>애플트리 건물주</p>
+          <time>1201.54.4564</time>
+          <address>
+            <p>강남구 어쩌고</p>
+          </address>
+          <span>1000원</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderHistoryDetailItem;
--- src/pages/order-hisotry/OrderHistoryPage.tsx
+++ src/pages/order-hisotry/OrderHistoryPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import OrderHistoryDetailItem from "../../components/domain/order-history/detail/OrderHistoryDetailItem";
 
 const OrderHistoryPage = () => {
-  return <div>주문 기록 페이지 입니다.</div>;
+  return (
+    <div>
+      주문 기록 페이지 입니다.
+      <OrderHistoryDetailItem />
+    </div>
+  );
 };
 
 export default OrderHistoryPage;
