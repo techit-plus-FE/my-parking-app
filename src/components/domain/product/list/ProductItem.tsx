0d7d8144ade292d3c066783151fb13b8eee42e33
Suam Kang <ksa199653@gmail.com>
2023-11-27 16:24:21 +0900

    Feat: 상품 목록 전체 조회 컴포넌트 기능 추가

--- src/components/domain/product/list/ProductItem.tsx
+++ src/components/domain/product/list/ProductItem.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-const ProductItem = () => {
-  return <div></div>;
+import classes from "./ProductItem.module.css";
+
+type Props = {
+  key: number;
+  product: ProductItemResponseType;
+};
+
+const ProductItem: React.FC<Props> = ({ product }) => {
+  // const { productId } = useParams<{ productId: string }>();
+
+  return (
+    <Link to={`/products/${product._id}`} className={classes["link-to-detail"]}>
+      <li className={classes.container}>
+        <div className={classes["product-imgBox"]}>
+          <img src={product.mainImages[0]} alt="게시글 사진" />
+        </div>
+        <div className={classes["product-info"]}>
+          <h4>{product.name}</h4>
+          <div className={classes["product-period"]}>
+            <small>
+              {product.extra?.periodFrom
+                ? product.extra?.periodFrom
+                : "2023.10.31"}{" "}
+              ~
+            </small>
+            <small>
+              {" "}
+              {product.extra?.periodTo ? product.extra?.periodTo : "2023.11.01"}
+            </small>
+          </div>
+          <p className={classes["product-price"]}>{product.price}원</p>
+        </div>
+      </li>
+    </Link>
+  );
 };
 
 export default ProductItem;
