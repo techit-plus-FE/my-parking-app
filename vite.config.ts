14f5e50f043b0de4f7df9dd70737d5f6faec0d21
Suam Kang <ksa199653@gmail.com>
2023-11-29 16:38:06 +0900

    Fix: 라우팅 경로 수정 및 body태그 overflow:hidden 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig(
+export default defineConfig({
   plugins: [
     react(),
-    svgr( svgrOptions: {
+    svgr({
+      svgrOptions: {
         // svgr options
       },
     }),
