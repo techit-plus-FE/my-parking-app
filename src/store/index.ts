92b294f6b0eeaea4101db2d5de641b8996322c00
Hyungeol Lee <ekfrhkquf12@naver.com>
2023-11-29 16:39:14 +0900

    Feat[MyPage]: 초기 세팅

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,7 @@
 
 import { create } from 'zustand'
 import { createAuthSlice } from './authSlice'
-// interface AuthSlice {
-//   signUp: (UserInput: UserInputType) => void;
-//   // logIn: 
-//   // 인증
-//   // 인가
-// }
+
 export const useBoundStore = create<AuthSlice>()((...a) => ({
   ...createAuthSlice(...a),
 }))
