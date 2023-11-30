a349c6431ea6b9ff06dc0cdc2827b19fa7be27d0
Hyungeol Lee <ekfrhkquf12@naver.com>
2023-11-30 16:27:18 +0900

    MyPage[feat]: MyPageSlice의 fetchAndSetMyInfo 기능 구현

--- src/store/MyPageSlice.tsx
+++ src/store/MyPageSlice.tsx
@@ -1,13 +1,48 @@
 // import { create } from "zustand";
 import axios from "axios"
 import {StateCreator} from 'zustand'
+import { BASE_URL } from "../services/BaseUrl"
+import { MyInfo, MyPageResponseType, MyPageSlice} from "../types/MyPage"
 
-//index.ts Store에서 AuthSlice를 참조하기 때문에 types 파일에 AuthSlice를 넣었습니다. 
+//index.ts Store에서 AuthSlice를 참조하기 때문에 types 파일에 AuthSlice type을 넣었습니다. 
 
-export const createAuthSlice: StateCreator<
+const requestMyInfo: (id: number) => Promise<MyInfo|undefined> = async (id: number) => {
+  // 서버로 회원가입 요청 보내기
+  try {
+        const response = await axios.get<number, MyPageResponseType>(
+          `${BASE_URL}/users/${id}`,
+        )
+        if (response.data.ok === 1) {
+          return response.data.item as MyInfo
+        } 
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   catch(Error: any) {
+    if (Error.response){
+      alert(Error.response.data.message)
+      }
+    console.error("Error:", Error);
+    }
+  }
+
+function isMyInfo(obj: MyInfo|undefined): obj is MyInfo {
+  // Iplement your type checing logic here
+  if (obj === undefined){
+    return false
+  }
+  return true
+}
+
+
+export const createMyPageSlice: StateCreator<
 MyPageSlice, 
-[]
+[]                                                                                                          
 > = (set) => ({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  personalInfo: ,
+  myInfo: {} as MyInfo,
+  fetchAndSetMyInfo: async (id) => {
+    const requestResult = await requestMyInfo(id)
+    if (isMyInfo(requestResult))
+      set(() => ({myInfo: requestResult}))
+  }
 })
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,12 @@
 // // combine할 slice도 정의
 
 import { create } from 'zustand'
-import { createAuthSlice } from './authSlice'
+import { createAuthSlice } from './AuthSlice'
 import { AuthSlice } from '../types/Auth'
+import { MyPageSlice} from '../types/MyPage'
+import {createMyPageSlice} from './MyPageSlice'
 
-export const useBoundStore = create<AuthSlice>()((...a) => ({
+export const useBoundStore = create<AuthSlice & MyPageSlice>()((...a) => ({
   ...createAuthSlice(...a),
+  ...createMyPageSlice(...a),
 }))
