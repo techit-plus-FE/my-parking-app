aca22966d76d2b064793abe400b2533e805db77e
Suam Kang <ksa199653@gmail.com>
2023-12-03 22:10:53 +0900

    Refactor: 모바일 사이즈 상품등록컴포넌트 폴더 이동

---
+++ src/components/domain/product/regist/mobile/FirstRegistForm.tsx
@@ -0,0 +1,46 @@
+// 첫번째 양식 컴포넌트
+// 카카오 지도 api로 등록할 주차장의 좌표 위치를 stirng값으로 전달받는다.
+import { useState } from "react";
+import KakaoMap from "../../../../common/map/KakaoMap";
+
+import classes from "./FirstRegistForm.module.css";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+  onNext: () => void;
+  onSubmit: (l: ProductLocationType) => void;
+};
+
+const FirstRegistForm = ({ onSubmit, onNext }: Props) => {
+  const navigate = useNavigate();
+  const [location, setLocation] = useState<ProductLocationType>({
+    address: "",
+    lat: "",
+    lng: "",
+  }); // 위치 정보를 저장할 상태 변수
+  console.log(location);
+
+  const handleNext = () => {
+    onSubmit(location); // 상태 변수를 전달하여 부모 컴포넌트의 onSubmit 함수 호출
+    onNext();
+  };
+  return (
+    <div className={classes.container}>
+      <h2>위치를 선택해 주세요.</h2>
+      <div className={classes["map-wrapper"]}>
+        {/* 카카오 지도에서 가져온 위경도 데이터를 받아와야함!!? */}
+        <KakaoMap setLocation={setLocation} />
+      </div>
+      <div className={classes.active}>
+        <button className={classes.prevBtn} onClick={() => navigate("/home")}>
+          이전
+        </button>
+        <button className={classes.nextBtn} onClick={handleNext}>
+          다음
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FirstRegistForm;
