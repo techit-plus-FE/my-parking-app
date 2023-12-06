1c6aaefd2fa39363f35b0b7d10b937312b728692
Suam Kang <ksa199653@gmail.com>
2023-12-06 15:29:58 +0900

    Fix:[Home] 기존 지도 컴포넌트 삭제

--- src/components/common/Home.tsx
+++ src/components/common/Home.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import ProductList from "../domain/product/list/ProductList";
 
 import classes from "./Home.module.css";
-import KakaoMap from "./map/KakaoMap";
 
 const Home = () => {
   // 임시 위치 상태 -> 여긴 setLocation이 필요없는 컴폰너트
@@ -11,8 +9,7 @@ const Home = () => {
   return (
     <div className={classes.container}>
       <div className={classes["map-box"]}>
-        {/* 검색해서 넘어가는 카카오 맵컴포넌트가 추가로 필요할까? */}
-        <KakaoMap />
+        {/* 메인 지도 컴포넌트가 있어야함*/}
       </div>
 
       <ProductList />
