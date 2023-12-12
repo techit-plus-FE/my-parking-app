7a74c3a2c0c0e4a47bad14b5ee210ea6972e4ddc
Suam Kang <ksa199653@gmail.com>
2023-12-12 19:48:46 +0900

    Fix:[Map] 카카오 지도 기능 초기값 좌표 받기 까지 머지

--- src/components/common/Home.tsx
+++ src/components/common/Home.tsx
@@ -9,57 +9,39 @@ import MainKakaoMap from "./map/MainKakaoMap";
 
 import LOGOBLUE from "../../assets/images/logo-blue.png";
 import Footer from "../layouts/Footer";
-import useCustomAxios from "../../services/useCustomAxios";
-
-// 1. 왼쪽 사이드바에서 검색어를 입력하면 지도에 표시되게 하려면 사이드바 컴포넌트에 props로 검색어 상태변경함수를 내려주어야함
-// 2. 검색된 위치에 해당하는 상품 데이터를 MainKakaoMap에 보여줘야하고, 해당하는 리스트를 불러오는건 오른족 사이드바 컴포넌트에서 진행해야함
-// 3. 지도 레벨에따라 해당하는 범위의 게시글을 랜더링해주기(정보업데이트)
 
 const Home = () => {
-  const axiosInstance = useCustomAxios();
-  const [products, setProducts] = useState<ProductListType | undefined>(); // 서버 요청 받는 상품들 데이터
-  // 검색창 인풋상태
+  const [map, setMap] = useState<kakao.maps.Map>();
+  const [products, setProducts] = useState<ProductListType | undefined>(); // 서버 요청 받는 상품들 데이터(초기, 검색후)
   const [searchValue, setSearchValue] = useState("");
-  const [map, setMap] = useState<kakao.maps.Map | undefined>();
 
-  const getProducts = async () => {
-    try {
-      const response = await axiosInstance.get<ProductListResType>(`/products`);
-      const responseData = response.data.item;
+  // 위치검색을 통한 지도 영역생성 함수
+  const handleSearchMakeMap = () => {
+    if (!map) return;
 
-      // console.log(responseData);
-      const listData = responseData;
-      setProducts(listData);
-    } catch (err) {
-      console.error("데이터를 불러오는데 문제가 생겼습니다.", err);
-    }
-  };
-
-  // 위치검색을 통한 지도 생성함수
-  const handleSearchMap = () => {
-    const ps = new kakao.maps.services.Places();
+    const ps = new kakao.maps.services.Places(map);
     const bounds = new kakao.maps.LatLngBounds(); // 지도 영역생성
 
     ps.keywordSearch(`${searchValue}`, function (result: any, status: any) {
       if (status === kakao.maps.services.Status.OK) {
-        const data = result[0]; // 가장 유사한 상위검색객체 저장
-        bounds.extend(new kakao.maps.LatLng(data.y, data.x)); // 받아온 검색한 결과값의 x,y좌표로 영역 재설정
-        map?.setBounds(bounds);
+        const data = result; // 가장 유사한 상위검색객체 저장
+        for (let i = 0; i < 3; i++) {
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x)); // 받아온 검색한 결과값의 x,y좌표로 영역 재설정
+        }
+        map.setBounds(bounds);
       }
     });
-    onReset();
-    return bounds;
+
+    return map.getBounds();
   };
 
-  const handleSearchAddress = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
   };
-  const onReset = () => {
-    setSearchValue("");
-  };
+
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
-      handleSearchMap();
+      handleSearchMakeMap();
     }
   };
 
@@ -73,11 +55,11 @@ const Home = () => {
         <div className={classes.searchBar}>
           <input
             type="text"
-            onChange={handleSearchAddress}
+            onChange={handleChange}
             onKeyDown={onKeyDown}
             value={searchValue || ""}
           />
-          <button onClick={handleSearchMap}>검색하기</button>
+          <button onClick={handleSearchMakeMap}>검색하기</button>
         </div>
         <div>
           <input type="date" />
@@ -91,8 +73,8 @@ const Home = () => {
         <MainKakaoMap
           map={map}
           setMap={setMap}
-          products={products}
-          handleSearchMap={handleSearchMap}
+          setProducts={setProducts}
+          handleSearchMakeMap={handleSearchMakeMap}
         />
       </div>
 
@@ -106,3 +88,7 @@ const Home = () => {
 export default Home;
 
 // 검색한 주소의 위치를 받아서 1. 지도가 이동하고 2. 받은 위치와 서버에 담긴 상품들을 불러와소 해당 상품들 위치정보와 같은 범위의 상품들을 마커배열에 등록해서 렌더링해준다.
+
+// 1. 왼쪽 사이드바에서 검색어를 입력하면 지도에 표시되게 하려면 사이드바 컴포넌트에 props로 검색어 상태변경함수를 내려주어야함
+// 2. 검색된 위치에 해당하는 상품 데이터를 MainKakaoMap에 보여줘야하고, 해당하는 리스트를 불러오는건 오른족 사이드바 컴포넌트에서 진행해야함
+// 3. 지도 레벨에따라 해당하는 범위의 게시글을 랜더링해주기(정보업데이트)
--- src/components/common/map/MainKakaoMap.tsx
+++ src/components/common/map/MainKakaoMap.tsx
@@ -7,18 +7,29 @@ import {
 } from "react-kakao-maps-sdk";
 
 import CustomOverlayBox from "./CustomOverlayBox";
+import { useBoundStore } from "../../../store";
 
 // 홈페이지 메인 지도 서비스
 type Props = {
   map: kakao.maps.Map | undefined;
   setMap: (m: kakao.maps.Map | undefined) => void;
-  products: ProductListType | undefined;
-  handleSearchMap: () => void;
+  setProducts: (l: ProductListType) => void;
+  handleSearchMakeMap: () => kakao.maps.LatLngBounds | undefined;
+  // searchValue: string;
 };
 
 // Home에서 내려준 props검색시 사용된 주소 받기
 
-const MainKakaoMap = ({ map, setMap, products, handleSearchMap }: Props) => {
+const MainKakaoMap = ({
+  map,
+  setMap,
+  setProducts,
+  handleSearchMakeMap,
+}: Props) => {
+  const searchItemsInThisBound = useBoundStore(
+    (state) => state.searchItemsInThisBound
+  );
+  const [mapExist, setMapExist] = useState<boolean>(false);
   const [location, setLocation] = useState({
     center: {
       lat: 37.5069632,
@@ -28,90 +39,27 @@ const MainKakaoMap = ({ map, setMap, products, handleSearchMap }: Props) => {
     isLoading: true,
   }); // 보여줄 위치상태
   const [markers, setMarkers] = useState<ProductListType | []>();
-  const [info, setInfo] = useState<InfoType | undefined>(); // 지도 정보
-  const [level, setLevel] = useState<number | undefined>(); // 지도 레벨
+  // const [info, setInfo] = useState<InfoType | undefined>(); // 지도 정보
+  const [level, setLevel] = useState<number | undefined>();
   const [isOverlayOpen, setIsOverlayOpen] = useState<boolean | undefined>(
     false
-  ); // 오버레이
-  const [selectedMarker, setSelectedMarker] = useState<number | null>(null); // 개별마커상태
+  );
+  const [selectedMarker, setSelectedMarker] = useState<number | null>(null);
 
   // 해당하는 bounds영역에 맞는 범위의 상품리스트 요청
   useEffect(() => {
-    const requsetSearchProduct = async () => {
-      const bounds = handleSearchMap();
-      // searchState 하기
-      const res = await searchItemsInThisBound(bounds);
-      setMarkers(res);
-    };
+    if (map && mapExist) {
+      const requsetSearchProduct = async () => {
+        const bound = handleSearchMakeMap();
+        const res = await searchItemsInThisBound(bound);
 
-    requsetSearchProduct();
-  }, [map]);
+        setMarkers(res); // 마커변경출력
+        setProducts(res); // 리스트변경출력
+      };
 
-  // 3. 실시간 현재위치에따른 지도 이동
-  useEffect(() => {
-    moveFirstLocation();
-  }, []);
-
-  // 4. 지도의 정보를 다시 받아오기
-  useEffect(() => {
-    handleMapInfo();
-  }, [map, location]);
-
-  // 첫 랜더링시 현재 접속 위치로 이동
-  const moveFirstLocation = () => {
-    if (navigator.geolocation) {
-      // geo서비스를 사용해서 접속 위치 추출
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setLocation((prev) => ({
-            ...prev,
-            center: {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            },
-            isLoading: false,
-          }));
-        },
-        (err) => {
-          setLocation((prev) => ({
-            ...prev,
-            errMsg: err.message,
-            isLoading: false,
-          }));
-        }
-      );
-    } else {
-      // geo서비스를 사용못할때 마커표시와 인포윈도우 내용 설정
-      setLocation((prev) => ({
-        ...prev,
-        errMsg: "실시간 위치정보를 불러오는데 문제가 생겼습니다.",
-        isLoading: false,
-      }));
+      requsetSearchProduct();
     }
-  };
-
-  // 주소 반경 정보 받아오기
-  const handleMapInfo = () => {
-    {
-      map &&
-        setInfo({
-          center: {
-            lat: map.getCenter().getLat(),
-            lng: map.getCenter().getLng(),
-          },
-          level: map.getLevel(),
-          typeId: map.getMapTypeId(),
-          swLatLng: {
-            lat: map.getBounds().getSouthWest().getLat(),
-            lng: map.getBounds().getSouthWest().getLng(),
-          },
-          neLatLng: {
-            lat: map.getBounds().getNorthEast().getLat(),
-            lng: map.getBounds().getNorthEast().getLng(),
-          },
-        });
-    }
-  };
+  }, [map, mapExist]);
 
   return (
     <>
@@ -122,9 +70,12 @@ const MainKakaoMap = ({ map, setMap, products, handleSearchMap }: Props) => {
         }}
         style={{ width: "100%", height: "100vh" }}
         level={level}
-        onCreate={(map) => setMap(map)}
+        onCreate={(map) => {
+          setMap(map); // 생성
+          setMapExist(true);
+        }}
         onZoomChanged={(map) => setLevel(map.getLevel())}
-        onIdle={handleMapInfo}
+        // onIdle={handleMapInfo}
       >
         {/* 1. 상품들 데이터리스트를 맵핑해서 해당 위치값을 마커로 보여주기 */}
         {markers &&
@@ -181,3 +132,69 @@ const MainKakaoMap = ({ map, setMap, products, handleSearchMap }: Props) => {
 };
 
 export default MainKakaoMap;
+
+// 3. 실시간 현재위치에따른 지도 이동
+// useEffect(() => {
+//   moveFirstLocation();
+// }, []);
+
+// 4. 지도의 정보를 다시 받아오기
+// useEffect(() => {
+//   handleMapInfo();
+// }, [map, location]);
+
+// 첫 랜더링시 현재 접속 위치로 이동
+// const moveFirstLocation = () => {
+//   if (navigator.geolocation) {
+//     // geo서비스를 사용해서 접속 위치 추출
+//     navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         setLocation((prev) => ({
+//           ...prev,
+//           center: {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude,
+//           },
+//           isLoading: false,
+//         }));
+//       },
+//       (err) => {
+//         setLocation((prev) => ({
+//           ...prev,
+//           errMsg: err.message,
+//           isLoading: false,
+//         }));
+//       }
+//     );
+//   } else {
+//     // geo서비스를 사용못할때 마커표시와 인포윈도우 내용 설정
+//     setLocation((prev) => ({
+//       ...prev,
+//       errMsg: "실시간 위치정보를 불러오는데 문제가 생겼습니다.",
+//       isLoading: false,
+//     }));
+//   }
+// };
+
+// // 주소 반경 정보 받아오기
+// const handleMapInfo = () => {
+//   {
+//     map &&
+//       setInfo({
+//         center: {
+//           lat: map.getCenter().getLat(),
+//           lng: map.getCenter().getLng(),
+//         },
+//         level: map.getLevel(),
+//         typeId: map.getMapTypeId(),
+//         swLatLng: {
+//           lat: map.getBounds().getSouthWest().getLat(),
+//           lng: map.getBounds().getSouthWest().getLng(),
+//         },
+//         neLatLng: {
+//           lat: map.getBounds().getNorthEast().getLat(),
+//           lng: map.getBounds().getNorthEast().getLng(),
+//         },
+//       });
+//   }
+// };
