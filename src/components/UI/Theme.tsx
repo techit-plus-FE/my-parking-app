6324eab834243aa4fefcfcf55880276a6f780a30
suconpa <thkWKddl100@gmail.com>
2023-12-12 11:50:42 +0900

    Style[mui]: 다크모드 로직 분리

---
+++ src/components/UI/Theme.tsx
@@ -0,0 +1,45 @@
+import React, { ReactNode } from "react";
+import { useBoundStore } from "../../store";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, createTheme } from "@mui/material";
+
+interface ThemeProps {
+  children: ReactNode;
+}
+
+const Theme: React.FC<ThemeProps> = ({ children }) => {
+  const setIsDark = useBoundStore((state) => state.setIsDark);
+  const isDark = useBoundStore((state) => state.isDark);
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: isDark ? "dark" : "light",
+      primary: {
+        main: "#90caf9", // 다크 모드에서의 primary 색상
+      },
+      secondary: {
+        main: "#ffcc80", // 다크 모드에서의 secondary 색상
+      },
+      background: {
+        default: isDark ? "#212121" : "#fff", // 기본 배경색
+        paper: isDark ? "#2c2c2c" : "#fff", // 다크 모드에서의 페이퍼 배경색
+      },
+      text: {
+        primary: isDark ? "#fff" : "#000", // 다크 모드에서의 텍스트 색상
+        secondary: "#989898", // 다크 모드에서의 보조 텍스트 색상
+      },
+
+      // 기타 색상들을 필요에 따라 추가할 수 있습니다.
+    },
+  });
+
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline>{children}</CssBaseline>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default Theme;
--- src/components/domain/product/detail/PriceAndBtnComponent.tsx
+++ src/components/domain/product/detail/PriceAndBtnComponent.tsx
@@ -4,7 +4,6 @@ import classes from "./PriceAndBtnComponent.module.css";
 import HEARTICON from "../../../../assets/images/heart-icon.png";
 import { Box } from "@mui/system";
 import { useBoundStore } from "../../../../store";
-import { CommonButtonSmall } from "../../../UI/CommonButton";
 
 const PriceAndBtnComponent = ({ product }: { product: ProductItemType }) => {
   const navigate = useNavigate();
@@ -12,6 +11,7 @@ const PriceAndBtnComponent = ({ product }: { product: ProductItemType }) => {
 
   return (
     <Box
+      className={classes.wrapper}
       sx={{
         backgroundColor: isDark ? "" : "var(--color-white)",
         display: "flex",
@@ -21,10 +21,10 @@ const PriceAndBtnComponent = ({ product }: { product: ProductItemType }) => {
         zIndex: 1002,
         width: "var(--main-max-width)",
         justifyContent: "space-around",
-        padding: "20px",
+        padding: "10px",
       }}
     >
-      <div className={(classes["util-box"], classes.wrapper)}>
+      <div className={classes["util-box"]}>
         <img src={HEARTICON} />
         <p>{product.price}원</p>
       </div>
