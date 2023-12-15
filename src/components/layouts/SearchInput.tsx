948f9ed8bf88fdde51098ed64899259c2f05e153
Hyungeol Lee <ekfrhkquf12@naver.com>
2023-12-15 11:33:20 +0900

    Feat: [Map] 기간 검색 기능 추가

--- src/components/layouts/SearchInput.tsx
+++ src/components/layouts/SearchInput.tsx
@@ -1,10 +1,10 @@
-import React, { ChangeEvent, useEffect, forwardRef, MutableRefObject } from "react";
+import React, { ChangeEvent, useEffect, useState, forwardRef, MutableRefObject } from "react";
 
 interface SearchInputProps {
   onKeywordChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   value?: string;
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   searchInfo: MapInfoType;
   setSearchInfo: (searchInfo: MapInfoType) => void;
 }
@@ -13,14 +13,20 @@ const SearchInput =
 forwardRef(function SearchInput(props: SearchInputProps, ref: React.ForwardedRef<HTMLInputElement>) {
   const { onKeywordChange,
     onKeyDown,
-    value,
     onClick,
     searchInfo,
     setSearchInfo} = props;
+  
+  const [period, setPeriod] = useState(['', ''])
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    onClick(e),
+    setSearchInfo({...searchInfo, period : period})
+    console.log(period)
+  }
 
   return (
     <div>
-      <input
+    <input
         type="text"
         onChange={onKeywordChange}
         onKeyDown={onKeyDown}
@@ -29,13 +35,19 @@ forwardRef(function SearchInput(props: SearchInputProps, ref: React.ForwardedRef
       />
       <input type="date"
       onChange = {(e)=> {
-        const formattedDate = (originalDate: Date)=>`${originalDate.getFullYear()}.${(originalDate.getMonth() + 1).toString().padStart(2, '0')}.${originalDate.getDate().toString().padStart(2, '0')}`
-        const period_start = formattedDate(new Date(e.target.value))
-        const period_end = formattedDate(new Date(e.target.value))
-        console.log(period_start, period_end)
-        setSearchInfo({...searchInfo, period : [period_start, period_end]});
+        // const formattedDate = (originalDate: Date)=>`${originalDate.getFullYear()}.${(originalDate.getMonth() + 1).toString().padStart(2, '0')}.${originalDate.getDate().toString().padStart(2, '0')}`
+        // const period_start = formattedDate(new Date(e.target.value))
+        // const period_end = formattedDate(new Date(e.target.value))
+        period[0] = e.target.value
       }}/>
-      <button onClick={onClick}>검색하기</button>
+      <input type="date"
+      onChange = {(e)=> {
+        // const formattedDate = (originalDate: Date)=>`${originalDate.getFullYear()}.${(originalDate.getMonth() + 1).toString().padStart(2, '0')}.${originalDate.getDate().toString().padStart(2, '0')}`
+        // const period_start = formattedDate(new Date(e.target.value))
+        // const period_end = formattedDate(new Date(e.target.value))
+        period[1] = e.target.value
+      }}/>
+      <button onClick={handleClick}>검색하기</button>
     </div>
   );
 });
