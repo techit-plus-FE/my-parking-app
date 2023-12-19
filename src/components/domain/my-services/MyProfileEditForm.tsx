5cc8e87085ec3e4b1fd20c846a9c9baf10d943e2
Hyungeol Lee <ekfrhkquf12@naver.com>
2023-12-19 23:24:16 +0900

    Style: [MyPage] MyProfileEditForm CSS 개선, module.css에 myProfileEditContainer 추가

--- src/components/domain/my-services/MyProfileEditForm.tsx
+++ src/components/domain/my-services/MyProfileEditForm.tsx
@@ -4,6 +4,7 @@ import Dialog from '@mui/material/Dialog';
 import React from "react";
 import DEFAULTIMAGE from '../../../assets/images/default-avatar.png'
 import { UserExtraInfo } from "../../../types/classImplementations";
+import classes from "./Mypage.module.css";
 
 interface MyProfileEditFormProps {
   myInfo: UserDetailInfoType
@@ -33,7 +34,7 @@ const MyProfileEditForm: React.FC<MyProfileEditFormProps>  = ({
   handleSubmit,
 }) => {
   return(
-  <Box>
+  <Box className={classes.myProfileEditContainer}>
       <div>
         {/*프로필 이미지 표시*/}
         <img src={`${myInfo.extra?.profileImage}`}/>
